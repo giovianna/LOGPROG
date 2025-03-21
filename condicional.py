4f54c06dba1f6a03762e51989af13f7e52260592
Giovanna Eduarda Viana <138938530+giovianna@users.noreply.github.com>
2025-03-21 20:29:25 -0300

    Create condicional.py

---
+++ condicional.py
@@ -0,0 +1,8 @@
+idade = int(input('Digite sua idade: '))
+
+if idade >= 18:
+  print('Você é maior de idade.')
+elif idade >= 16:
+  print('Você pode votar, mas não pode dirigir.')
+else:
+  print('Você é menor de idade.')
